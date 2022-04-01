bc00345376671cb67f2edcfc058f4e1786f3c02e
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-04-01 15:30:45 +0900

    pogi

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, Request
 import pandas as pd
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 

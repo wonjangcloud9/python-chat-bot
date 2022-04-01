bfc4545908b14ef03071b416b16d8dbaf3faa6b0
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-04-01 15:07:48 +0900

    add template

--- main.py
+++ main.py
@@ -1,60 +1,16 @@
-from fastapi import FastAPI
-import streamlit as st
-from streamlit_chat import message
+from fastapi import FastAPI, Request
 import pandas as pd
-from sentence_transformers import SentenceTransformer
-from sklearn.metrics.pairwise import cosine_similarity
-import json
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 
-#
-# app = FastAPI()
-#
-#
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
-
-
-@st.cache(allow_output_mutation=True)
-def cached_model():
-    model = SentenceTransformer('jhgan/ko-sroberta-multitask')
-    return model
-
-
-@st.cache(allow_output_mutation=True)
-def get_dataset():
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
     df = pd.read_csv('wellness_dataset.csv')
-    df['embedding'] = df['embedding'].apply(json.loads)
-    return df
-
-
-model = cached_model()
-df = get_dataset()
-
-st.header('심리상담 챗봇')
-st.markdown("[❤️빵형의 개발도상국](https://www.youtube.com/c/빵형의개발도상국)")
-
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
-
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
-
-with st.form('form', clear_on_submit=True):
-    user_input = st.text_input('당신: ', '')
-    submitted = st.form_submit_button('전송')
-
-if submitted and user_input:
-    embedding = model.encode(user_input)
-
-    df['distance'] = df['embedding'].map(lambda x: cosine_similarity([embedding], [x]).squeeze())
-    answer = df.loc[df['distance'].idxmax()]
-
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(answer['챗봇'])
-
-for i in range(len(st.session_state['past'])):
-    message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-    if len(st.session_state['generated']) > i:
-        message(st.session_state['generated'][i], key=str(i) + '_bot')
+    print(df)
+    print(df.iloc[1][1])
+    return templates.TemplateResponse("test.html", {"request": request, "data": df.iloc[1][1]})
